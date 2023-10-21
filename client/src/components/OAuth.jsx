b97b40afb73c4f76a094d65e685d7962e1e0bce7
Oluwatobi-23534 <oluwatobi23534@gmail.com>
2023-10-21 16:33:17 +0100

    final touch

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,18 +1,18 @@
-import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth";
-import { app } from "../firebase";
+import { GoogleAuthProvider, signInWithPopup, getAuth } from 'firebase/auth';
+import { app } from '../firebase';
 import { useDispatch } from 'react-redux';
-import { signInSuccess } from "../redux/user/userSlice";
-import { useNavigate } from 'react-router-dom'
+import { signInSuccess } from '../redux/user/userSlice';
+import { useNavigate } from 'react-router-dom';
 
 export default function OAuth() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const handleGoogleClick = async () => {
     try {
+      const provider = new GoogleAuthProvider();
+      const auth = getAuth(app);
 
-      const provider = new GoogleAuthProvider()
-      const auth = getAuth(app)
-      const result = await signInWithPopup(auth, provider)
+      const result = await signInWithPopup(auth, provider);
       const res = await fetch('/api/auth/google', {
         method: 'POST',
         headers: {
@@ -21,17 +21,24 @@ export default function OAuth() {
         body: JSON.stringify({
           name: result.user.displayName,
           email: result.user.email,
-          photo: result.user.photoUrl,
-        })
+          photo: result.user.photoURL,
+        }),
       });
       const data = await res.json();
+      console.log(data);
       dispatch(signInSuccess(data));
       navigate('/');
     } catch (error) {
-      console.log("could not log in with google", error);
+      console.log('could not login with google', error);
     }
-  }
+  };
   return (
-    <button type="button" onClick={handleGoogleClick} className="bg-red-700 text-white rounded-lg p-3 uppercase hover:opcaity-95">Continue with google</button>
-  )
+    <button
+      type='button'
+      onClick={handleGoogleClick}
+      className='bg-red-700 text-white rounded-lg p-3 uppercase hover:opacity-95'
+    >
+      Continue with google
+    </button>
+  );
 }
